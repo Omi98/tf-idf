27343d26f8f28804fa5678ee81671086edb34adc
soloham <monosoft54@gmail.com>
2020-12-06 07:40:57 +0500

    feat: tfidf, cosine similarity & frontend

--- fyp/tf_idf/urls.py
+++ fyp/tf_idf/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     # website pages URLs
     
     # website actions URLs
-    path('api/generate_model/', views.generate_model_custom),
     path('api/search_articles/', views.search_articles)
 ]
--- fyp/tf_idf/views.py
+++ fyp/tf_idf/views.py
@@ -10,114 +10,55 @@ import pandas as pd
 from .models import Papers
 from .models import RecommendationModel
 from sklearn.metrics.pairwise import cosine_similarity
-
-# Create your views here.
-def computeTFIDF(tfBagOfWords, idfs):
-    tfidf = {}
-    for word, val in tfBagOfWords.items():
-        tfidf[word] = val * idfs[word]
-    return tfidf
-
-def computeIDF(documents):
-    import math
-    N = len(documents)
-    
-    idfDict = dict.fromkeys(documents[0].keys(), 0)
-    for document in documents:
-        for word, val in document.items():
-            if val > 0:
-                idfDict[word] += 1
-    
-    for word, val in idfDict.items():
-        idfDict[word] = math.log(N / float(val))
-    return idfDict
-
-def computeTF(wordDict, bagOfWords):
-    tfDict = {}
-    bagOfWordsCount = len(bagOfWords)
-    for word, count in wordDict.items():
-        tfDict[word] = count / float(bagOfWordsCount)
-    return tfDict
-
-def generate_model_custom(request):
-    if request.user.is_authenticated:
-        # data = pd.read_csv(os.path.join(BASE_DIR, 'tf_idf/models/Papers Data.csv'),encoding='latin1')  
-
-        # for idx, row in data.iterrows():
-        #     exists = Papers.objects.filter(paper_id=row[0]).exists() # return True/False
-        #     if not exists:
-        #         author_keywords = row[2]
-        #         abstract = row[3]
-        #         # documents.append(abstract)
-        #         _, created = Papers.objects.get_or_create(
-        #             paper_id=row[0],
-        #             paper_title=row[1],
-        #             author_keywords=author_keywords,
-        #             abstract=abstract,
-        #             area=row[4],
-        #         )
-        
-        documents = []
-        papers = Papers.objects.all()
-        for paper in papers:
-            document = paper.paper_title + " " + paper.abstract
-            documents.append(document)
-
-        uniquewords = []
-        for doc in documents:
-            parsed_doc = doc.split(' ')
-            uniquewords = set(parsed_doc).union(set(uniquewords))
-
-        wordsCountDict = []
-        tfDict = []
-        for idx, doc in enumerate(documents):
-            parsed_doc = doc.split(' ')
-            numOfWords = dict.fromkeys(uniquewords, 0)
-            for word in parsed_doc:
-                numOfWords[word] += 1
-            wordsCountDict.append(numOfWords);
-            tf = computeTF(numOfWords, parsed_doc)
-            tfDict.append(tf)
-
-        idfs = computeIDF(wordsCountDict)
-
-        for tf in tfDict:
-            tfIdf = computeTFIDF(tf, idfs)
-
-        # 3: User Query
-        paper_title = request.GET.get('paper_title')
-        keywords = request.GET.get('keywords')
-        abstract = request.GET.get('abstract')
-        area = request.GET.get('area')
-
-        queryDocument = paper_title + " " + abstract
-        
-        parsed_doc = queryDocument.split(' ')
-        queryNumOfWords = dict.fromkeys(uniquewords, 0)
-        for word in parsed_doc:
-            queryNumOfWords[word] += 1
-
-        wordsCountDict = [queryNumOfWords]
-        tfDict = computeTF(queryNumOfWords, parsed_doc)
-        print(tfDict)
-
-        Idfs = computeIDF(wordsCountDict)
-
-        queryTfIdf = computeTFIDF(tfDict, Idfs)
-        print(queryTfIdf)
-
-        return render(request, 'search.html')
-    else:
-        return redirect('/login')
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 def search_articles(request):
     if request.user.is_authenticated:
-        paper_title = request.GET.get('paper_title')
+        # 3: User Query
+        area = request.GET.get('area')
+        search = request.GET.get('search')
         keywords = request.GET.get('keywords')
         abstract = request.GET.get('abstract')
-        area = request.GET.get('area')
-        print("tf_idf ->", paper_title, keywords, abstract, area)
-        
-        return render(request, 'search.html')
+
+        if area is None and search is None and keywords is None and abstract is None:
+            return render(request, 'search.html')
+
+        documents = []
+        papers = Papers.objects.all()
+
+        papers_docs_mapping = []
+        for paper in papers:
+            if paper.area == area:
+                document = paper.paper_title + " " + paper.abstract + " " + paper.author_keywords
+                documents.append(document)
+                papers_docs_mapping.append({ 'paper': paper, 'document': document })
+
+        vectorizer = TfidfVectorizer()
+        vectors = vectorizer.fit_transform(documents)
+
+        feature_names = vectorizer.get_feature_names()
+        dense = vectors.todense()
+        denselist = dense.tolist()
+
+
+        queryDocument = search + " " + abstract + " " + keywords
+
+        query_vec = vectorizer.transform([queryDocument])
+
+        results = cosine_similarity(vectors,query_vec)
+
+        score_doc_map = [];
+        for idx, score  in enumerate(results):
+            score_doc_map.append({ 'score': score, 'index': idx })
+
+        score_doc_map.sort(key=lambda x: x['score'], reverse=True)
+
+        sorted_papers = []
+        for sorted_doc_idx in score_doc_map:
+            doc = documents[sorted_doc_idx['index']]
+            paper_map = next((item for item in papers_docs_mapping if item["document"] == doc), None)
+            sorted_papers.append(paper_map['paper'])
+
+        return render(request, 'search.html', { 'sorted_papers': sorted_papers, 'area': area })
     else:
         return redirect('/login')
